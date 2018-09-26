30f2ed29017317cfbae0186fe2739513be23c51f
Computer Architecture <xue.camille@gmail.com>
2018-09-26 19:17:06 -0400

    changed variable names

--- adder.t.v
+++ adder.t.v
@@ -8,19 +8,96 @@ module testFull4BitAdder();
     wire carryout, overflow;
     reg carryin;
 
-    //behavioralFullAdder adder (sum, carryout, a, b, carryin);
+
     FullAdder4bit adder (sum[3:0], carryout, overflow, a, b);
     initial begin
 
-    $display("A     B     | Cout  Sum "); //actually Cout here might just be overflow?
-    a[3]=1; a[2]=0;a[1]=1;a[0]=0;
+    // Test Cases with two positive numbers, no overflow
+    $display("Test Cases with 2 Positive Numbers, No Overflow");
+    $display("A     B     | Sum   Exp. | Cout  Exp. | Over  Exp.  ");
+    a[3]=0; a[2]=0;a[1]=0;a[0]=0;
+    b[3]=0; b[2]=1;b[1]=1;b[0]=0;
+    carryin=0; #1000
+    $display("%b  %b  | %b  0110 | %b     0    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=0; a[2]=0;a[1]=1;a[0]=1;
+    b[3]=0; b[2]=1;b[1]=0;b[0]=0;
+    carryin=0; #1000
+    $display("%b  %b  | %b  0110 | %b     0    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=0; a[2]=0;a[1]=1;a[0]=1;
+    b[3]=0; b[2]=1;b[1]=0;b[0]=0;
+    carryin=0; #1000
+    $display("%b  %b  | %b  0111 | %b     0    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    $display("-------------------------------------------------");
+
+    //Test Cases with two positives, with overflow
+    $display("Test Cases with 2 Positive Numbers, With Overflow");
+    $display("A     B     | Sum   Exp. | Cout  Exp. | Over  Exp.  ");
+    a[3]=0; a[2]=1;a[1]=0;a[0]=1;
+    b[3]=0; b[2]=0;b[1]=1;b[0]=1;
+    carryin=0; #1000
+    $display("%b  %b  | %b  1000 | %b     0    | %b     1 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=0; a[2]=1;a[1]=1;a[0]=0;
+    b[3]=0; b[2]=1;b[1]=1;b[0]=0;
+    carryin=0; #1000
+    $display("%b  %b  | %b  1100 | %b     0    | %b     1 ", a, b[3:0], sum, carryout, overflow);
+    $display("-------------------------------------------------");
+
+    // Test Cases with two negatives and no overflow
+    $display("Test Cases with 2 Negative Numbers, No Overflow");
+    $display("A     B     | Sum   Exp. | Cout  Exp. | Over  Exp.  ");
+    a[3]=1; a[2]=1;a[1]=1;a[0]=0;
+    b[3]=1; b[2]=1;b[1]=0;b[0]=0;
+    carryin=0; #1000
+    $display("%b  %b  | %b  1010 | %b     1    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=1; a[2]=0;a[1]=1;a[0]=1;
+    b[3]=1; b[2]=1;b[1]=1;b[0]=0;
+    carryin=0; #1000
+    $display("%b  %b  | %b  1001 | %b     1    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=1; a[2]=1;a[1]=1;a[0]=1;
+    b[3]=1; b[2]=1;b[1]=0;b[0]=1;
+    carryin=0; #1000
+    $display("%b  %b  | %b  1100 | %b     1    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    $display("-------------------------------------------------");
+
+    // Test Cases with two negatives, with overflow
+    $display("Test Cases with 2 Negative Numbers, With Overflow");
+    $display("A     B     | Sum   Exp. | Cout  Exp. | Over  Exp.  ");
+    a[3]=1; a[2]=1;a[1]=1;a[0]=0;
     b[3]=1; b[2]=0;b[1]=0;b[0]=1;
     carryin=0; #1000
-    $display("%b  %b  |  %b    %b  | ", a, b[3:0], carryout, sum);
-    a[3]=1; a[2]=0;a[1]=1;a[0]=0;
-    b[3]=0; b[2]=1;b[1]=0;b[0]=1;
+    $display("%b  %b  | %b  0111 | %b     1    | %b     1 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=1; a[2]=0;a[1]=0;a[0]=0;
+    b[3]=1; b[2]=0;b[1]=1;b[0]=1;
     carryin=0; #1000
-    $display("%b  %b  |  %b    %b  | ", a, b[3:0], carryout, sum);
+    $display("%b  %b  | %b  0011 | %b     1    | %b     1 ", a, b[3:0], sum, carryout, overflow);
+    $display("-------------------------------------------------");
+
+    // Test Cases with positive and negative, and carryout of 1
+    $display("Test Cases with Negative & Positive Numbers, with Carryout");
+    $display("A     B     | Sum   Exp. | Cout  Exp. | Over  Exp.  ");
+    a[3]=1; a[2]=1;a[1]=1;a[0]=1;
+    b[3]=0; b[2]=1;b[1]=1;b[0]=1;
+    carryin=0; #1000
+    $display("%b  %b  | %b  0110 | %b     1    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=0; a[2]=0;a[1]=1;a[0]=1;
+    b[3]=1; b[2]=1;b[1]=1;b[0]=1;
+    carryin=0; #1000
+    $display("%b  %b  | %b  0010 | %b     1    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    $display("-------------------------------------------------");
+
+    // Test Cases with positive and negative, no carryout
+    $display("Test Cases with Positive & Negative Numbers, No Carryout");
+    $display("A     B     | Sum   Exp. | Cout  Exp. | Over  Exp.  ");
+    a[3]=0; a[2]=0;a[1]=1;a[0]=0;
+    b[3]=1; b[2]=0;b[1]=0;b[0]=1;
+    carryin=0; #1000
+    $display("%b  %b  | %b  1011 | %b     0    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    a[3]=0; a[2]=0;a[1]=0;a[0]=0;
+    b[3]=1; b[2]=0;b[1]=0;b[0]=0;
+    carryin=0; #1000
+    $display("%b  %b  | %b  1000 | %b     0    | %b     0 ", a, b[3:0], sum, carryout, overflow);
+    $display("-------------------------------------------------");
+
     $finish();
     end
 endmodule
--- adder.v
+++ adder.v
@@ -30,10 +30,11 @@ module FullAdder4bit
   wire carryout0, carryout1, carryout2, carryout3;
   reg carryin = 0;
 
-  structuralFullAdder adder1 (sum[0], carryout0, a[0], b[0], carryin);
-  structuralFullAdder adder2 (sum[1], carryout1, a[1], b[1], carryout0);
-  structuralFullAdder adder3 (sum[2], carryout2, a[2], b[2], carryout1);
-  structuralFullAdder adder4 (sum[3], carryout, a[3], b[3], carryout2);
+  structuralFullAdder adder0 (sum[0], carryout0, a[0], b[0], carryin);
+  structuralFullAdder adder1 (sum[1], carryout1, a[1], b[1], carryout0);
+  structuralFullAdder adder2 (sum[2], carryout2, a[2], b[2], carryout1);
+  structuralFullAdder adder3 (sum[3], carryout, a[3], b[3], carryout2);
 
+  `XOR xorgate(overflow, carryout, carryout2);
 
   endmodule
